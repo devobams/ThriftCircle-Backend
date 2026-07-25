cdee4acb63c0ca38843b55ccc1febcfd167be4a9
ebelethe <ebelethes@gmail.com>
2026-07-24 20:03:15 -0700

    [groups] dropped hardcoded /groups prefix (added once at mount time); added authorize ('organizer') to POST /groups

--- src/modules/groups/groups.routes.js
+++ src/modules/groups/groups.routes.js
@@ -1,11 +1,12 @@
 
 import { Router } from "express";
 import { authenticate } from "../../middleware/authenticate.js";
+import { authorize } from "../../middleware/authorize.js";
 import { handleCreateGroup, handleGetGroupById } from "./groups.controller.js";
 
 const router = Router();
 
-router.post("/groups", authenticate, handleCreateGroup);
-router.get("/groups/:id", authenticate, handleGetGroupById);
+router.post("/", authenticate, authorize("organizer"), handleCreateGroup);
+router.get("/:id", authenticate, handleGetGroupById);
 
 export default router;
