96209843fba840d5fa554f021a20106bab52bb34
Cherechi_Emmanuel <cherechidimobika@gmail.com>
2026-08-04 12:35:22 +0100

    Added Validation requirements For zod schema

--- src/modules/auth/auth.validation.js
+++ src/modules/auth/auth.validation.js
@@ -28,3 +28,36 @@ export const loginSchema = z.object({
   phone_number: z.string().min(10),
   password: z.string().min(6),
 });
+
+
+export const forgotPasswordSchema = z.object({
+  phone_number: z
+    .string()
+    .min(10, "Phone number must be at least 10 digits"),
+});
+
+
+export const verifyResetOtpSchema = z.object({
+  phone_number: z
+    .string()
+    .min(10, "Phone number must be at least 10 digits"),
+
+  otp: z
+    .string()
+    .length(6, "OTP must be exactly 6 digits"),
+});
+
+
+export const resetPasswordSchema = z.object({
+  phone_number: z
+    .string()
+    .min(10, "Phone number must be at least 10 digits"),
+
+  otp: z
+    .string()
+    .length(6, "OTP must be exactly 6 digits"),
+
+  new_password: z
+    .string()
+    .min(6, "Password must be at least 6 characters"),
+});
